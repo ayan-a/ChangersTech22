00060ff7cd7023692449d89de96f7856d677a466
ayan-a <108087412+ayan-a@users.noreply.github.com>
2022-06-23 19:19:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// let myH1 = document.querySelector("h1");
+// console.log(myH1);
+// console.log("Vart är konsolen?")
+// myH1.innerText = "Changers Tech 22 - Javascript edition"
+
+// Event listener
+let showMoreBtn = document.querySelector("#show-more");
+console.log(showMoreBtn)
+
+showMoreBtn.addEventListener()
